e600021f6545b1f7b5a61d7f1223c2d285b2bd81
arvind <developer.arvind003@gmail.com>
2023-02-05 14:10:12 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useEffect ,useState} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData]= useState([]);
+  useEffect(()=>{
+    fetch("https://api.spaceflightnewsapi.net/v3/articles").then((response)=>{
+        response.json().then((apiData)=>{
+          setData(apiData)
+        })
+    })
+  },[])
   return (
     <div className="App">
-      
+    <div className="title">
+      <h2>Space News Api- project</h2>
+    </div>
+    <div className="newContainer">
+
+    {data.map((val)=>{
+      return <div key={val.id} className="article" onClick={()=>{window.location.href=val.url}}> 
+                  <h3>
+                    {val.title}
+                  </h3>
+                    <img src={val.imageUrl} alt="Loading....." />
+                    <p>{val.summary}</p>
+                     <h4> <strong style={{fontSize:"20px"}}>Date : </strong>{val.publishedAt.substring(0,10)}</h4>
+                    
+              </div>
+    })}
+    </div>
     </div>
   );
 }
